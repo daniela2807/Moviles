6805585811b81a21f23e2f9aa11732afe3cd293d
Daniela Gutierrez <you@example.com>
2020-11-24 18:54:57 -0600

    bitmap

--- app/src/main/java/com/example/examenfinal/MainActivity2.kt
+++ app/src/main/java/com/example/examenfinal/MainActivity2.kt
@@ -1,23 +1,36 @@
 package com.example.examenfinal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
+
 
 class MainActivity2 : AppCompatActivity() {
 
     lateinit var fotografia: ImageView
+   lateinit var sepia: Button
+    lateinit var bitmap: Bitmap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         val image = intent.getStringExtra("image")
-        Toast.makeText(applicationContext," "+image, Toast.LENGTH_SHORT).show()
+        Toast.makeText(applicationContext, " " + image, Toast.LENGTH_SHORT).show()
         fotografia = findViewById(R.id.fotografia)
+        sepia = findViewById(R.id.button)
+       // fotografia2 = findViewById(R.id.fotografia2)
         if (image != null) {
             fotografia.setImageURI(image.toUri())
+            bitmap = (fotografia.getDrawable() as BitmapDrawable).bitmap
+            fotografia.setImageBitmap(bitmap)
         }
+        //efecto()
     }
+
 }
